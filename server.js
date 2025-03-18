75bb2cbef06a3c938016bb56994445513f564eea
dimi1729 <dimi@tamu.edu>
2025-03-17 22:09:41 -0500

    wip mongo db

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/mongodb');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
+
+connectDB();
 
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Backend is connected!' });
