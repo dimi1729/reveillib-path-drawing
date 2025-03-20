0092de8d077937d81f0aa00b726d406f9018ced1
dimi1729 <dimi@tamu.edu>
2025-03-19 20:20:57 -0500

    added some basic code generation stuff

--- frontend/src/components/code_blocks.js
+++ frontend/src/components/code_blocks.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './code_blocks.css';
+import { codeGenerator } from '../helpers/code_generator'
 
-function CodeBlocks({ text }) {
+function CodeBlocks({ x_ft, y_ft, speed }) {
+
+  console.log(`x feet is ${x_ft}`)
+  console.log(`y feet is ${y_ft}`)
+  console.log(`speed is ${speed}`)
+
   return (
     <div className="code-blocks-container">
       <pre className="code-block code-block-content">
         {"// ADD COMMENTS HERE"}
       </pre>
       <pre className="code-block code-block-content">
-        {text}
+        {codeGenerator(x_ft, y_ft, speed)}
       </pre>
       <pre className="code-block code-block-content">
-        {"reckless->await()"}
+        {"reckless->await();"}
       </pre>
     </div>
   );
