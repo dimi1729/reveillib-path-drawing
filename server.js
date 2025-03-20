c2b49655a139ac0934f300a8e4f9f81b6b56584f
JSim011235 <JustinbSimms@gmail.com>
2025-03-20 14:32:35 -0500

    firebase test works

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const userRoutes = require('./routes/userRoutes');
-// app.use('/api/users', userRoutes);
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
 
 
 
