67d5fa95b1b524b115df163b496b256017cf4d74
JSim011235 <JustinbSimms@gmail.com>
2025-03-21 02:43:35 -0500

    Preliminary Firebase Real Time Data Base Test
    RTDB works and sets data, need to add authentication and expand functionality for real time and setting data anywhere in the react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import ImageComponent from './components/image_selector';
+import fbConfig from './configuration/firebase';
+import { getDatabase, ref, onValue, set } from "firebase/database";
 
 function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
+    const db = getDatabase(fbConfig);
+    console.log(db);
+    set(ref(db, 'users/' + "test"), {
+      username: "test",
+      email: "test",
+      profile_picture : "test"
+    });
+
     fetch('/api/test')
       .then(response => response.json())
       .then(data => setMessage(data.message));
