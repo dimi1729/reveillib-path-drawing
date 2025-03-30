02b6f5f7449c7059c8971d7563376ffab2513522
dimi1729 <dimi@tamu.edu>
2025-03-30 12:04:35 -0500

    fixed mongodb removal

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const userRoutes = require('./routes/userRoutes');
-app.use('/api/users', userRoutes);
-
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Backend is connected!' });
 });
