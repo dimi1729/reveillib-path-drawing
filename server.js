2140e4cf00e0677f4e10f4ca40676723a99d3ef6
dimi1729 <dimi@tamu.edu>
2025-03-30 12:01:36 -0500

    removed mongodb stuff

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const connectDB = require('./config/mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,8 +10,6 @@ app.use(express.json());
 
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
-
-connectDB();
 
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Backend is connected!' });
